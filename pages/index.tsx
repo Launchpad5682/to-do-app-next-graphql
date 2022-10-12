9bd9afba08ce67ce3432dee41a3947c108efa245
launchpad5682 <launchpad5682@gmail.com>
2022-10-13 00:18:57 +0530

    fix: check for ci test

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Bye to <a href="https://nextjs.org">Next.js!</a>
+          Welocome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
